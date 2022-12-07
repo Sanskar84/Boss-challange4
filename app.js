7d363725603a805af83d8f754efbf0fe4594eda0
Sanskar Bharadia <bittubharadia@gmail.com>
2022-12-07 18:36:38 +0530

    SECURED DB

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const dotevn = require("dotenv");
+
+dotevn.config({path:'./config.env'});
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,7 +19,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin_Sanskar:sanskar84@cluster0.pnxksap.mongodb.net/postsDB?retryWrites=true&w=majority", {useNewUrlParser: true});
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB).then(()=> {
+  console.log('connection successful'); 
+}).catch((err)=> console.log('no connection'));
+
+
 
 const postSchema = {
   title: String,
@@ -76,6 +85,6 @@ app.get("/contact", function(req, res){
 
 
 const port = process.env.PORT;
-app.listen(port, function () {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
