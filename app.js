4175b855f1a5868055e67a1dca77617e5812fe67
Sanskar Bharadia <bittubharadia@gmail.com>
2022-12-07 15:31:52 +0530

    second-commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin_Sanskar:<password>@cluster0.pnxksap.mongodb.net/postsDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin_Sanskar:sanskar84@cluster0.pnxksap.mongodb.net/postsDB?retryWrites=true&w=majority", {useNewUrlParser: true});
 
 const postSchema = {
   title: String,
