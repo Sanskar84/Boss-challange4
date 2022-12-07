310dbc3713fd739ebccfd5d6c54db7de9e4f0280
Sanskar Bharadia <bittubharadia@gmail.com>
2022-12-07 19:02:00 +0530

    fix error

--- app.js
+++ app.js
@@ -85,6 +85,6 @@ app.get("/contact", function(req, res){
 
 
 const port = process.env.PORT;
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
